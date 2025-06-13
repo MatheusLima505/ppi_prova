c3d2eefbea896fd654748910a1b22259764f6476
MatheusLima505 <mathlegeferi@gmail.com>
2025-06-13 19:41:48 -0300

    links update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
         <div className="footer-container">
             <div className='footer-ifrn'><p>IFRN - Campus Macau</p> <p>Curso Técnico em Informática</p><p>Programação para a Internet 2025</p></div>
             <div className='bonheur-royale-regular'>Guilherme Leal Santos</div>
-            <div className='footer-icons'><a href=""><Github /></a><a href=""><Instagram /></a><a href=""><Phone /></a></div>
+            <div className='footer-icons'><a href="https://github.com/MatheusLima505"><Github /></a><a href="https://naotenhoinstragram.com"><Instagram /></a><a href="https://paginadecontatoquenaoexiste.com"><Phone /></a></div>
         </div>
     )
 }
